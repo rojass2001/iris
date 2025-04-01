256ca04e843c3e65642dcec693f7e8014740b8b6
rojass2001 <rojascherian@gmail.com>
2025-04-01 18:05:19 +0530

    third commit

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,7 +3,7 @@ function Banner() {
   const gradient="bg-gradient-to-r from-pink-600 via-purple-500 to-pink-500"
   return (
     <div className='w-full'>
-      <p className={`text-4xl  font-bold  animate-pulse bg-clip-text text-transparent ${gradient}`}>Hi I am Iris.</p>
+      <p className={`text-4xl  font-bold  animate-pulse bg-clip-text text-transparent ${gradient}`}>Hi I ams Iris.</p>
       <p className={`text-3xl mt-7 mb-6 font-bold animate-bounce bg-clip-text text-transparent ${gradient}`}>Hello! How can I help you today?</p>
  
     </div>
