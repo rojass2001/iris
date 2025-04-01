e412122a214264be73daa08dd00c1b314f899f30
rojass2001 <rojascherian@gmail.com>
2025-04-01 20:42:07 +0530

    third commit

--- src/components/Ansandquestiongrid.tsx
+++ src/components/Ansandquestiongrid.tsx
@@ -5,8 +5,8 @@ const gradient="bg-gradient-to-r from-pink-600 via-purple-500 to-pink-500"
 function Ansandquestiongrid() {
   const{question,answer}=useSelector((state:Rootstate)=>state.chat)
   return (
-    <div className='w-full h-screen overflow-y-auto px-2 italic  pb-[100px]'>
-    <h3 className={`text-3xl mb-5 font-bold text-end font-sans  bg-clip-text text-transparent ${gradient}`}>{question}</h3>
+    <div className='w-full h-screen overflow-y-auto px-2  pb-[100px]'>
+     <h3 className={`text-3xl mb-5 font-bold text-end font-sans  bg-clip-text text-transparent ${gradient}`}>{question}</h3>
     <div  className={`w-full text-xl italic font-bold bg-clip-text text-transparent ${gradient}`}>{answer}</div>
    </div>
   )
